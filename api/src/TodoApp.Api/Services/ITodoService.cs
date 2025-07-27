450021440161e0cd55bc17a26d451cba7f7b41bc
qeqqe <armansingh3172006@gmail.com>
2025-07-27 23:55:40 +0530

    Feat(Service): Add TodoService methods

--- api/src/TodoApp.Api/Services/ITodoService.cs
+++ api/src/TodoApp.Api/Services/ITodoService.cs
@@ -6,8 +6,8 @@ namespace TodoApp.Api.Services
     {
         Task<IEnumerable<Todo>> GetAllTodosAsync();
         Task<Todo?> GetTodoByIdAsync(int id);
-        Task<Todo> CreateTodoAsync(CreateTodoDto createTodo);
-        Task<Todo?> UpdateTodoAsync(UpdateTodoDto updateTodo);
+        Task<Todo> CreateTodoAsync(CreateTodoDto createDto);
+        Task<Todo?> UpdateTodoAsync(int id, UpdateTodoDto updateDto);
         Task<bool> DeleteTodoAsync(int id);
         Task<Todo?> ToggleCompletionAsync(int id);
     }
--- api/src/TodoApp.Api/Services/TodoService.cs
+++ api/src/TodoApp.Api/Services/TodoService.cs
@@ -4,36 +4,72 @@ namespace TodoApp.Api.Services
 {
     public class TodoService : ITodoService
     {
-        private readonly Todo _todo = new();
+        private readonly List<Todo> _todos = new();
         private int _nextid = 1;
-        public Task<Todo> CreateTodoAsync(CreateTodoDto createTodo)
+
+        public async Task<IEnumerable<Todo>> GetAllTodosAsync()
         {
-            throw new NotImplementedException();
+            await Task.Delay(1);
+            return _todos.OrderByDescending(t => t.CreatedAt);
         }
 
-        public Task<bool> DeleteTodoAsync(int id)
+        public async Task<Todo?> GetTodoByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            await Task.Delay(1);
+            return _todos.Find(t => t.Id == id);
         }
 
-        public Task<IEnumerable<Todo>> GetAllTodosAsync()
+        public async Task<Todo> CreateTodoAsync(CreateTodoDto createDto)
         {
-            throw new NotImplementedException();
+            await Task.Delay(1);
+            var todo = new Todo
+            {
+                Id = _nextid++,
+                Title = createDto.Title,
+                Description = createDto.Description,
+                IsCompleted = false,
+                CreatedAt = DateTime.UtcNow
+            };
+            _todos.Add(todo);
+            return todo;
         }
 
-        public Task<Todo?> GetTodoByIdAsync(int id)
+        public async Task<Todo?> UpdateTodoAsync(int id, UpdateTodoDto updateDto)
         {
-            throw new NotImplementedException();
+            await Task.Delay(1);
+            // this is the references of the object in _todos with the Id = id, not a copy of that value.
+            var todo = _todos.Find(t => t.Id == id);
+            if (todo == null) return null;
+            todo.Title = updateDto.Title;
+            todo.Description = updateDto.Description;
+
+            todo.IsCompleted = (!todo.IsCompleted && updateDto.IsCompleted) ? true : false;
+            todo.CompletedAt = (!todo.IsCompleted && updateDto.IsCompleted) ? DateTime.UtcNow : null;
+
+            return todo;
         }
 
-        public Task<Todo?> ToggleCompletionAsync(int id)
+        public async Task<bool> DeleteTodoAsync(int id)
         {
-            throw new NotImplementedException();
+            await Task.Delay(1);
+            var todo = _todos.Find(t => t.Id == id);
+            if (todo == null) return false;
+
+            _todos.Remove(todo);
+
+            return true;
         }
 
-        public Task<Todo?> UpdateTodoAsync(UpdateTodoDto updateTodo)
+        public async Task<Todo?> ToggleCompletionAsync(int id)
         {
-            throw new NotImplementedException();
-        }
+            await Task.Delay(1);
+
+            var todo = _todos.Find(t => t.Id == id);
+            if (todo == null) return null;
+
+            todo.IsCompleted = !todo.IsCompleted;
+            todo.CompletedAt = todo.IsCompleted ? DateTime.UtcNow : null;
+            return todo;
+        }    
     }
 }
